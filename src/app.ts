83cc73509ff3a50d59c8bd42434b2cc1d41ccaca
Jeff See <jeffsee.55@gmail.com>
2025-10-31 11:12:10 -0700

    Update app to not use `api` basepath

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import {Hono} from "hono";
+import { Hono } from "hono";
 
-const app = new Hono().basePath('/api')
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
-})
-export default app
+app.get("/", (c) => {
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
+});
+export default app;
